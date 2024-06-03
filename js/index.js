e6553aa9b03f97ebab5d52c250f204e5888ec5b5
hiroakioike <bbt76aki@gmail.com>
2024-06-03 13:46:07 +0900

    js　タスク追加

--- js/index.js
+++ js/index.js
@@ -20,107 +20,68 @@ const todoList = [
 ];
 
 // JSファイルが読み込まれたときにtodoListをHTMLに表示する
-// HTMLに表示するまでの流れ
-// 1. HTMLのどの場所に表示するかを取得する
-// 2. 表示したいHTMLの形式に合わせてDOM要素を生成する
-// 3. 1で取得した場所に2で生成したDOM要素を追加する
-
 const displayTodoList = () => {
   //   1. HTMLのどの場所に表示するかを取得する
   // tbody要素を取得
   const todoListElement = document.getElementById("todoList");
 
-  // 2. 表示したいHTMLの形式に合わせてDOM要素を生成する
-  // 2-1. todoListの0番目のデータを取得
-  const todo = todoList[0];
-  console.log(todoList[0]);
+  // 表示時に一度displayの中身を消す（その後追加削除されたもの含め表示）
+  while (todoListElement.firstChild) {
+    todoListElement.removeChild(todoListElement.firstChild);
+  }
+  // // 2. 表示したいHTMLの形式に合わせてDOM要素を生成する
+  // // 2-1. todoListの0,1,2番目のデータを取得
+  todoList.forEach((todo) => {
+    // 他の書き方
+    // todoList.map((todo) => {
+    // for (const todo of todoList) {
+    // for (let i = 0; i < todoList.length; i++) {
+    //   const todo = todoList[i];
 
-  // 2-2. <tr>要素を生成
-  const trElement = document.createElement("tr");
-  console.log("trElement:", trElement);
+    //   // 2-2. <tr>要素を生成
+    const trElement = document.createElement("tr");
 
-  // 2-3. <tr>要素の中に配置する<td>要素を生成
-  const idElement = document.createElement("td");
-  const taskNameElement = document.createElement("td");
-  const limitElement = document.createElement("td");
-  const statusElement = document.createElement("td");
+    // 2-3. <tr>要素の中に配置する<td>要素を生成
+    const idElement = document.createElement("td");
+    const taskNameElement = document.createElement("td");
+    const limitElement = document.createElement("td");
+    const statusElement = document.createElement("td");
 
-  // 2-4. <td>要素の中にtodoListの0番目のデータを表示する
-  idElement.textContent = todo.id;
-  taskNameElement.textContent = todo.taskName;
-  limitElement.textContent = todo.limit;
-  statusElement.textContent = todo.status;
+    // 2-4. <td>要素の中にtodoListの0番目のデータを表示する
+    idElement.textContent = todo.id;
+    taskNameElement.textContent = todo.taskName;
+    limitElement.textContent = todo.limit;
+    statusElement.textContent = todo.status;
 
-  // 2-5. <tr>要素の中に<td>要素を追加
-  trElement.appendChild(idElement);
-  trElement.appendChild(taskNameElement);
-  trElement.appendChild(limitElement);
-  trElement.appendChild(statusElement);
+    // 2-5. <tr>要素の中に<td>要素を追加
+    trElement.appendChild(idElement);
+    trElement.appendChild(taskNameElement);
+    trElement.appendChild(limitElement);
+    trElement.appendChild(statusElement);
 
-  // 3. 1で取得した場所に2で生成したDOM要素を追加する
-  todoListElement.appendChild(trElement);
-
-  // 以下、1番目のデータ
-  // 2. 表示したいHTMLの形式に合わせてDOM要素を生成する
-  // 2-1. todoListの1番目のデータを取得
-  const todo1 = todoList[1];
-  console.log(todoList[1]);
-
-  // 2-2. <tr>要素を生成
-  const trElement1 = document.createElement("tr");
-  console.log("trElement:", trElement1);
-
-  // 2-3. <tr>要素の中に配置する<td>要素を生成
-  const idElement1 = document.createElement("td");
-  const taskNameElement1 = document.createElement("td");
-  const limitElement1 = document.createElement("td");
-  const statusElement1 = document.createElement("td");
-
-  // 2-4. <td>要素の中にtodoListの1番目のデータを表示する
-  idElement1.textContent = todo1.id;
-  taskNameElement1.textContent = todo1.taskName;
-  limitElement1.textContent = todo1.limit;
-  statusElement1.textContent = todo1.status;
-
-  // 2-5. <tr>要素の中に<td>要素を追加
-  trElement1.appendChild(idElement1);
-  trElement1.appendChild(taskNameElement1);
-  trElement1.appendChild(limitElement1);
-  trElement1.appendChild(statusElement1);
-
-  // 3. 1で取得した場所に2で生成したDOM要素を追加する
-  todoListElement.appendChild(trElement1);
-
-  // 以下、2番目のデータ
-  // 2. 表示したいHTMLの形式に合わせてDOM要素を生成する
-  // 2-1. todoListの0番目のデータを取得
-  const todo2 = todoList[2];
-  console.log(todoList[2]);
-
-  // 2-2. <tr>要素を生成
-  const trElement2 = document.createElement("tr");
-  console.log("trElement:", trElement2);
-
-  // 2-3. <tr>要素の中に配置する<td>要素を生成
-  const idElement2 = document.createElement("td");
-  const taskNameElement2 = document.createElement("td");
-  const limitElement2 = document.createElement("td");
-  const statusElement2 = document.createElement("td");
-
-  // 2-4. <td>要素の中にtodoListの0番目のデータを表示する
-  idElement2.textContent = todo2.id;
-  taskNameElement2.textContent = todo2.taskName;
-  limitElement2.textContent = todo2.limit;
-  statusElement2.textContent = todo2.status;
-
-  // 2-5. <tr>要素の中に<td>要素を追加
-  trElement2.appendChild(idElement2);
-  trElement2.appendChild(taskNameElement2);
-  trElement2.appendChild(limitElement2);
-  trElement2.appendChild(statusElement2);
-
-  // 3. 1で取得した場所に2で生成したDOM要素を追加する
-  todoListElement.appendChild(trElement2);
+    // 3. 1で取得した場所に2で生成したDOM要素を追加する
+    todoListElement.appendChild(trElement);
+  });
 };
 
 displayTodoList();
+
+// 1.追加ボタン押したらconsole吐き出す
+// 2.追加ボタン押したら配列にデータ追加（こんそーるでわかればおｋ
+// 3.追加ボタン押したら配列にデータ追加してHTMLに表示
+// 4.追加ボタン押したら入力フォームに入力された文字列がこんそーるに表示
+// 5.タスク名でHTMLに表示される
+const addBtn = document.getElementById("btn-add");
+addBtn.addEventListener("click", () => {
+  const taskNameElement = document.getElementById("taskName");
+  const taskDateElement = document.getElementById("taskDate");
+
+  todoList.push({
+    id: todoList.length + 1,
+    taskName: taskNameElement.value,
+    limit: taskDateElement.value,
+    status: "作業中",
+  });
+
+  displayTodoList();
+});
